69a42fee39fa3c9c0365e1453fb1e7450c31420a
buremba <emrekabakci@gmail.com>
2021-05-21 20:08:22 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -177,7 +177,7 @@ module.exports = {
       //   src: 'img/oss_logo.png',
       //   href: 'https://rakam.io',
       // },
-      copyright: `From the makers of [rakam](https://rakam.io)`,
+      copyright: `From the makers of <a href="https://rakam.io">rakam</a>`,
     },
   },
   presets: [
