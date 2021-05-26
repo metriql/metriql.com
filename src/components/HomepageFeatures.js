fada4575c695dd14bb00863776068a8ba0645ff8
buremba <emrekabakci@gmail.com>
2021-05-26 10:59:25 +0300

    Update HomepageFeatures.js

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -8,7 +8,7 @@ const FeatureList = [
     // Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Metriql integrates with <a target="_blank" href="https://getdbt.com">dbt</a> and creates datasets from your dbt resources. It also creates automatic dbt roll-up models to save data teams time.
+        Metriql integrates with <a target="_blank" href="https://getdbt.com">dbt</a> and maps dbt resources as [datasets](/tutorial/for-dbt-users). It can also create automatic dbt roll-up models to save data teams' time.
       </>
     ),
   },
