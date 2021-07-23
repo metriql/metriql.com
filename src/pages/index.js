b4e99da89031c3e41e046c79d00dbfd3ed37544a
Emre Semercioglu <emrehesapg@gmail.com>
2021-07-23 21:07:28 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
-import { ReactComponent as HomepageDiagram } from '../components/homepage-diagram.svg';
+import { ReactComponent as HomepageDiagram } from '../components/homepage-diagram2.svg';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
