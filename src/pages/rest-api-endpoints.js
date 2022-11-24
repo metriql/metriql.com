15988ec07452427372c8f056bd53f4037931eee4
Burak Emre Kabakcı <emrekabakci@gmail.com>
2022-11-24 18:28:35 +0000

    Update rest-api-endpoints.js

--- src/pages/rest-api-endpoints.js
+++ src/pages/rest-api-endpoints.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import "rapidoc";
 
 // const STATIC_SPEC = 'https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/examples/v2.0/json/petstore-simple.json';
 const STATIC_SPEC = 'schema/openapi.json';
@@ -9,6 +8,8 @@ const STATIC_SPEC = 'schema/openapi.json';
 function CustomPage() {
   return (
     <Layout title="Open API Docs" description="Open API Reference Docs for API">
+          <script type="module" src="https://unpkg.com/rapidoc/dist/rapidoc-min.js"></script>
+
       <rapi-doc
       style={{ height: "100vh", width: "100%" }}
       spec-url="http://localhost:3000/schema/openapi.json"
